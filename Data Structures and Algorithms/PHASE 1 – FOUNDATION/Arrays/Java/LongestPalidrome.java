3c94489db80ba7686dfff321f440fe1fa26a067f
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-04-13 10:08:19 +0800

    Longest Palindrome problem solved

---
+++ Data Structures and Algorithms/PHASE 1 – FOUNDATION/Arrays/Java/LongestPalidrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int sLength = s.length();
+        int[] freq = new int[58];
+        int palindromeLength = 0;
+        for (int i = 0; i < sLength; i++) {
+            int index = s.charAt(i) - 'a';
+            ++freq[index];
+        }
+        for (int i : freq) {
+            palindromeLength += (i / 2) * 2;
+        }
+        if (palindromeLength < sLength) ++palindromeLength;
+        return palindromeLength;
+    }
+}
