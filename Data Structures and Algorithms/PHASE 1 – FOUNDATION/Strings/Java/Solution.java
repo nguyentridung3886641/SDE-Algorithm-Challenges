bb17f0ebd0bb3691ece4262e254f293aab5978f2
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-04-15 11:03:20 +0800

    Weighted uniform String problem solved

---
+++ Data Structures and Algorithms/PHASE 1 – FOUNDATION/Strings/Java/Solution.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'weightedUniformStrings' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER_ARRAY queries
+     */
+
+    public static List<String> weightedUniformStrings(String s, List<Integer> queries) {
+        HashSet<Integer> weightSet = new HashSet<>();
+        ArrayList<String> result = new ArrayList<>();
+        int sLength = s.length();
+        int subWeight = 0;
+        for (int i = 0; i < sLength; i++) {
+            subWeight += s.charAt(i) - 'a' + 1;
+            weightSet.add(subWeight);
+            if (i == sLength - 1) break;
+            if (s.charAt(i) != s.charAt(i + 1))
+                subWeight = 0;
+        }
+        for (int q : queries) {
+            if (weightSet.contains(q))
+                result.add("Yes");
+            else
+                result.add("No");
+        }
+        return result;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<String> result = Result.weightedUniformStrings(s, queries);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
--- README.md
+++ README.md
@@ -70,7 +70,7 @@ _Mục tiêu là nắm vững các dạng bài phổ biến trong phỏng vấn 
 | 37  | 14/04 | [Is Subsequence](https://leetcode.com/problems/is-subsequence/)                                                                                                |  LeetCode  |  🟢 Easy   |      [View](./Data%20Structures%20and%20Algorithms/PHASE%201%20–%20FOUNDATION/Arrays/Java/ReverseWordsinaStringIII.java)       |
 | 38  | 14/04 | [Mars Exploration](https://www.hackerrank.com/challenges/mars-exploration/problem)                                                                             | HackerRank |  🟢 Easy   |          [View](./Data%20Structures%20and%20Algorithms/PHASE%201%20–%20FOUNDATION/Strings/Java/MarsExploration.java)           |
 | 39  | 14/04 | [Caesar Cipher](https://www.hackerrank.com/challenges/caesar-cipher-1/problem)                                                                                 | HackerRank |  🟢 Easy   |               [View](./Data%20Structures%20and%20Algorithms/PHASE%201%20–%20FOUNDATION/Strings/C/CaesarCipher.c)               |
-| 40  | 14/04 | [HackerRank in a String](https://www.hackerrank.com/challenges/caesar-cipher-1/problem)                                                                        | HackerRank |  🟢 Easy   |        [View](./Data%20Structures%20and%20Algorithms/PHASE%201%20–%20FOUNDATION/Strings/Java/HackerRankinaString!.java)        |
+| 40  | 14/04 | [HackerRank in a String](https://www.hackerrank.com/challenges/hackerrank-in-a-string/problem)                                                                        | HackerRank |  🟢 Easy   |        [View](./Data%20Structures%20and%20Algorithms/PHASE%201%20–%20FOUNDATION/Strings/Java/HackerRankinaString!.java)        |
 
 </details>
 
