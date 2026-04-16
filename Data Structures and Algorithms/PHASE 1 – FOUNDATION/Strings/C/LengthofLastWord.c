09b55dbb644645320bfee3802d02e04301515ef5
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-04-16 14:54:29 +0800

    Length of Last Word problem solved

--- Data Structures and Algorithms/PHASE 1 – FOUNDATION/Strings/C/LengthofLastWord.c
+++ Data Structures and Algorithms/PHASE 1 – FOUNDATION/Strings/C/LengthofLastWord.c
@@ -2,8 +2,9 @@ int lengthOfLastWord(char* s) {
     int sLength = strlen(s);
     int i = sLength - 1;
     int wordLength = 0;
-    while (s[i] == ' ' && i >= 0) --i;
-    while (s[i] != ' ' && i >= 0) {
+    while (i >= 0 && s[i] == ' ') 
+        --i;
+    while (i >= 0 && s[i] != ' ') {
         ++wordLength;
         --i;
     }
