3a8ab1f15e73a0f22804bff48b5d2f9ee8aed172
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-04-23 00:10:06 +0800

    Making Anagrams problem solved

---
+++ Data Structures and Algorithms/PHASE 1 – FOUNDATION/Strings/C/MakingAnagrams.c
@@ -0,0 +1,109 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'makingAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+int makingAnagrams(char* s1, char* s2) {
+    int s1Length = strlen(s1);
+    int s2Length = strlen(s2);
+    int freq[26] = {0};
+    int res = 0;
+    for (int i = 0; i < s1Length; i++) {
+        ++freq[s1[i] - 'a'];
+    }
+    for (int i = 0; i < s2Length; i++) {
+        --freq[s2[i] - 'a'];
+    }
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            res += abs(freq[i]);
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s1 = readline();
+
+    char* s2 = readline();
+
+    int result = makingAnagrams(s1, s2);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
