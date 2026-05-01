3ce2410bacc22354b15b6be1741565529c317e65
NguyenTriDung-Java-SDE <nguyentridung3886641@gmail.com>
2026-05-01 16:23:18 +0700

    Sort Colors problem solved

---
+++ Data Structures and Algorithms/PHASE 1 – FOUNDATION/Arrays/Java/SortColors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int numsLength = nums.length;
+        int left = 0;
+        int right = numsLength - 1;
+        int i = 0;
+        while (i <= right) {
+            if (nums[i] == 0) {
+                swap(left, i, nums);
+                left++;
+                i++;
+            }
+            else if (nums[i] == 2) {
+                swap(right, i, nums);
+                right--;
+            }
+            else i++;
+        }
+    }
+}
